6173b6afe944f6e03e6c24b2446ca3f4a8004ea5
Osiris <artem.kozakevych@gmail.com>
2026-07-26 00:45:17 +0300

    Fix

--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -3,18 +3,11 @@ package core.basesyntax;
 import java.util.Random;
 
 public class Lottery {
-    private Ball ball = new Ball();
 
     public Ball getRandomBall() {
+        Ball ball = new Ball();
         ball.setColor(new ColorSupplier().getRandomColor());
         ball.setNumber(new Random().nextInt(100));
         return ball;
-    }
-
-    @Override
-    public String toString() {
-        return "Lottery{"
-               + "ball=" + ball
-               + '}';
     }
 }
