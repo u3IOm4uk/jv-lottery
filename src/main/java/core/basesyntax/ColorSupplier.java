6640a97314e38fcc387bafeedd4c827d28f0a23f
Osiris <artem.kozakevych@gmail.com>
2026-07-26 00:29:49 +0300

    Fix

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -3,9 +3,12 @@ package core.basesyntax;
 import java.util.Random;
 
 public class ColorSupplier {
+    Random random = new Random();
+    Color color;
+
     public String getRandomColor() {
-        int index = new Random().nextInt(Color.values().length);
-        Color color = Color.values()[index];
-        return color.toString();
+        int index = random.nextInt(Color.values().length);
+        color = Color.values()[index];
+        return color.name();
     }
 }
---
+++ src/main/java/core/basesyntax/Lottery.java
@@ -0,0 +1,20 @@
+package core.basesyntax;
+
+import java.util.Random;
+
+public class Lottery {
+    Ball ball = new Ball();
+
+    public Ball getRandomBall() {
+        ball.setColor(new ColorSupplier().getRandomColor());
+        ball.setNumber(new Random().nextInt(100));
+        return ball;
+    }
+
+    @Override
+    public String toString() {
+        return "Lottery{" +
+                "ball=" + ball +
+                '}';
+    }
+}
